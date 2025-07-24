8052dbf2008ee93ca7c98a32c6965c7cd475c20b
Levchenko Artem <evgn.main@gmail.com>
2025-07-24 22:59:14 +0300

    main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/shevgn/simplebank/api"
+	db "github.com/shevgn/simplebank/db/sqlc"
+	"github.com/shevgn/simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	defer conn.Close()
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Run(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+}
