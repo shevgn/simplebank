b6b2e6fb05787d2ed73ed7aafcefddf8dbc3622f
Levchenko Artem <evgn.main@gmail.com>
2025-07-29 18:29:03 +0300

    randomUser function

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -12,8 +12,11 @@ import (
 
 // createRandomAccount creates random account for testing
 func createRandomAccount(t *testing.T) Account {
+
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomBalance(),
 		Currency: util.RandomCurrency(),
 	}
